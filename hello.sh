79761fc8a4cc2cb36183a7e45b83c60f44b71c9e
Shaik Abdul Vahid <59871200+SAW-NAAZ@users.noreply.github.com>
2023-12-22 10:00:45 +0530

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 echo "Hello and welcome to the session be regulaer"
-
+#!/bin/bash
+echo "Hello and welcome to the session be regulaer"
+#!/bin/bash
+echo "Hello and welcome to the session be regulaer"
+#!/bin/bash
+echo "Hello and welcome to the session be regulaer"
+#!/bin/bash
+echo "Hello and welcome to the session be regulaer"
+#!/bin/bash
+echo "Hello and welcome to the session be regulaer"
